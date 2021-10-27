b39b3e9b9e4079a42fd9acb46aa4297983ae2a0a
marat.rif <marat.rif@salvationarmy.ca>
2021-10-27 16:29:52 -0400

    update menu-item to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.components';
+import {Switch,Route} from 'react-router-dom';
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE </h1>
+  </div>
+);
 function App() {
   return (
     <div>
-     <HomePage></HomePage>
+     <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
