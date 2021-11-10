98c3f7172569a7022cb174e4388740991b1d9ddd
marat.rif <marat.rif@salvationarmy.ca>
2021-11-10 13:41:09 -0500

    finishing submit method

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import './sign-in.styles.scss';
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
-import { signInWithGoogle } from "../../firebase/firebase.utils";
+import {auth, signInWithGoogle } from "../../firebase/firebase.utils";
 
 class SignIn extends React.Component{
     constructor(props){
@@ -16,9 +16,16 @@ class SignIn extends React.Component{
         const {value, name} = event.target;
         this.setState({[name]:value});
     }
-    handleSubmit = (event) =>{
+    handleSubmit = async event =>{
         event.preventDefault();
-        this.state({email:'',password:''})
+        const {email, password} = this.state;
+        try{
+            auth.signInWithEmailAndPassword(email,password);
+            this.setState({email:'',password:''})
+        }catch(error){
+            console.log(error);
+        }
+      
     }
     render(){
         return (
