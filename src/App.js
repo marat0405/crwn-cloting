7ff4795e2707a7df68efe1d98392f59615e8f5a4
Marat Rif <Marat@kwizcom.com>
2021-11-15 14:55:30 -0500

    updated to redirect to home if user signed in

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.components';
-import {Switch,Route} from 'react-router-dom';
+import {Switch,Route,Redirect} from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
@@ -42,7 +42,7 @@ class App extends React.Component {
        <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
-          <Route path='/signin' component={SignInAndSignUpPage} />
+          <Route exact path='/signin' render = {() => this.props.currentUser ? (<Redirect to = '/' />):(<SignInAndSignUpPage />)} />
         </Switch>
       </div>
     );
@@ -50,7 +50,10 @@ class App extends React.Component {
  
 }
 
+const mapStateToProps = ({user}) =>({
+  currentUser: user.currentUser
+})
 const mapDispathToProps = dispatch =>({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 });
-export default connect(null,mapDispathToProps)(App);
+export default connect(mapStateToProps,mapDispathToProps)(App);
